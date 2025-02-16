ae9c23d9545bef3fb62ba0f8a0b5ecd4df9c363e
Tomas Toledo <trtoledo1977@gmail.com>
2025-02-15 22:59:02 -0300

    links

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { Route, Route } from 'react-router-dom';
+import { Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+     
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        </div>
+      <div id="main-section">
+      <Routes>
+       <Route path="/blue" element={<h1>Blue</h1>} />
+       <Route path="/red" element={<h1>Red</h1>} />    
+      </Routes>
+        </div>
       <Routes></Routes>
     </div>
-  );
+  )
 }
 
-export default App
+export default App;
